d77bcf3ab0b5ec1a7e1a941b20e7f687066b7382
Olena Zhogol <lenazhogol@gmail.com>
2025-10-01 19:42:44 +0300

    updated after refactoring objects.js and loop.js

--- lesson4/src/loop.js
+++ lesson4/src/loop.js
@@ -1,29 +1,54 @@
-for (let i = 0; i <= 9; i++) {
+console.log("for - 0 to 9:");
+for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
-let d = 100;
-while (d >= 0) {
-    console.log(d);
-    d -= 10;
+console.log("break-continue 0 to 9:");
+for (let l = 0; l < 10; l++) {
+    if (l === 4) {
+        continue;
+    }
+    console.log(l);
+    if (l === 6) {
+        break;
+    }
 }
 
-//Part for training purposes only
-//for (let i = 0; i < 10; i++) {
-//    if (i === 4) {
-//        continue;
-//    }
-//    console.log(i);
-//    if (i === 8) {
-//        break;
-//   }
-//}
-//let randomNum;
-//let attempts = 0; // лічильник спроб
+console.log("for...of - 100 to 0 with step 10:");
+const hundreds = [];
+for (let j = 100; j >= 0; j -= 10) {
+    hundreds.push(j);
+}
+for (const v of hundreds) {
+    console.log(v);
+}
 
-//do {
-//    randomNum = Math.floor(Math.random() * 10) + 1; // число від 1 до 10
-//    attempts++; // збільшуємо кількість спроб
-//    console.log(`Спроба ${attempts}: згенеровано ${randomNum}`);
-//} while (randomNum !== 7);
+console.log("while 0 to 9:");
+let k = 0;
+while (k < 10) {
+    console.log(k);
+    k++;
+}
 
+console.log("do...while 100 to 0 with step 10:");
+let d = 100;
+do {
+    console.log(d);
+    d -= 10;
+} while (d >= 0);
+
+console.log("forEach 0 to 9:");
+const zeroToNine = [];
+for (let m = 0; m < 10; m++) {
+    zeroToNine.push(m);
+}
+zeroToNine.forEach(function (value) {
+    console.log(value);
+});
+
+console.log("map 100 to 0 with step 10:");
+const sameHundreds = hundreds.map(function (value) {
+    console.log(value);
+    return value;
+});
+console.log("sameHundreds array:", sameHundreds);
--- lesson4/src/objects.js
+++ lesson4/src/objects.js
@@ -10,16 +10,23 @@ obj.profile = {
         phone: '+3806343913**'
     },
     skills: ['Chaos management', 'Testing', 'Test automation'],
-    hobbies: ['reading', 'crocheting', 'cats']
+    hobbies: ['reading', 'crocheting', 'cats'],
+    summary: function() {
+        const fullName = `${this.name.first} ${this.name.last}`;
+        console.log(`Name: ${fullName}`);
+        console.log(`Email: ${this.contacts.email}`);
+        console.log(`Skills: ${this.skills.join(', ')}`);
+        console.log(`Hobbies: ${this.hobbies.join(', ')}`);
+    }
 };
 
+obj.profile.summary();
 console.log(obj);
-console.log(JSON.stringify(obj));
 
-//Part for training purposes only
-//obj.profile.skills.forEach(skill => {
-//    console.log('Skill:', skill);
-//});
-//obj.profile.hobbies.forEach(hobby => {
-//    console.log('Hobby:', hobby);
-//});
+
+obj.profile.skills.forEach(skill => {
+    console.log('Skill:', skill);
+});
+obj.profile.hobbies.forEach(hobby => {
+    console.log('Hobby:', hobby);
+});
