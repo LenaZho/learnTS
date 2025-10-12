839f59a0a0d755d71fe1bbd8452d57ec9a5643f6
Olena Zhogol <lenazhogol@gmail.com>
2025-10-12 13:09:10 +0300

    updates for lesson7 after review

--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,13 +1,16 @@
 function addArrayElements(arr: (number | string)[]): number {
     let sum = 0;
     for (const item of arr) {
-        sum += Number(item);
+        const num = Number(item);
+        if (!isNaN(num)) {
+            sum += num;
+        }
     }
     return sum;
 }
 
 const numbers: number[] = [2, 4, 6, 8, 10];
-const strings: string[] = ['1', '3', '5', '7', '9'];
+const strings: string[] = ['1', 'three', '5', '7', 'nine', 'no number'];
 
 console.log('Sum of numbers:', addArrayElements(numbers));
 console.log('Sum of strings:', addArrayElements(strings));
